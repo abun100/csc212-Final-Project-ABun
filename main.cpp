e3111538c5641f37f3f9d754a1767263a81ca410
abun100 <114605559+abun100@users.noreply.github.com>
2022-11-22 22:03:04 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 struct Point
@@ -62,8 +63,8 @@ int distSq(Point p1, Point p2)
 // 2 --> Counterclockwise
 int orientation(Point p, Point q, Point r)
 {
-	int val = (q.x - p.x) * (r.y - q.y) -
-		(q.y - p.y) * (r.x - q.x);
+	int val = (q.y - p.y) * (r.x - q.x) -
+		(q.x - p.x) * (r.y - q.y);
 
 	if (val == 0) return 0; // collinear
 	return (val > 0) ? 1 : 2; // clock or counterclock wise
@@ -89,89 +90,103 @@ void convexHull(std::vector<Point> &points)
 {
 	std::vector<Point> temp;
 	// Find the bottommost point
-	int xmax = points[0].x, min = 0;
+	int ymin = points[0].y, min = 0;
 	for (int i = 1; i < points.size(); i++)
 	{
-		int x = points[i].x;
+		int y = points[i].y;
 
 		// Pick the bottom-most or choose the left
 		// most point in case of tie
-		if (((x > xmax) || (xmax == x &&
-			points[i].y < points[min].y)) &&
+		if (((y < ymin) || (ymin == y &&
+			points[i].x < points[min].x)) &&
 			points[i].color == 0)
-			xmax = points[i].x, min = i;
+			ymin = points[i].y, min = i;
 	}
 
 	// Place the bottom-most point at first position
-	swap(points[0], points[min]);
+swap(points[0], points[min]);
 
-	// Sort n-1 points with respect to the first point.
-	// A point p1 comes before p2 in sorted output if p2
-	// has larger polar angle (in counterclockwise
-	// direction) than p1
-	p0 = points[0];
-	temp.push_back(points[0]);
-	qsort(&points[1], points.size() - 1, sizeof(Point), compare);
+// Sort n-1 points with respect to the first point.
+// A point p1 comes before p2 in sorted output if p2
+// has larger polar angle (in counterclockwise
+// direction) than p1
+p0 = points[0];
+temp.push_back(points[0]);
+qsort(&points[1], points.size() - 1, sizeof(Point), compare);
 
-	// If two or more points make same angle with p0,
-	// Remove all but the one that is farthest from p0
-	// Remember that, in above sorting, our criteria was
-	// to keep the farthest point at the end when more than
-	// one points have same angle.
-	
-	for (int i = 1; i < points.size(); i++)
-	{
-		// Keep removing i while angle of i and i+1 is same
-		// with respect to p0
-		while ((i < points.size() - 1 && orientation(p0, points[i],
-			points[i + 1]) == 0))
-			i++;
+// If two or more points make same angle with p0,
+// Remove all but the one that is farthest from p0
+// Remember that, in above sorting, our criteria was
+// to keep the farthest point at the end when more than
+// one points have same angle.
 
-		if (points[0].color == 1) {
-			continue;
-		}
+for (int i = 1; i < points.size(); i++)
+{
+	// Keep removing i while angle of i and i+1 is same
+	// with respect to p0
+	while ((i < points.size() - 1 && orientation(p0, points[i],
+		points[i + 1]) == 0))
+		i++;
 
-		temp.push_back(points[i]);
-		
+	if (points[i].color == 1) {
+		continue;
 	}
 
-	for (auto& elem : temp) {
-		std::cout << elem.x << ":" << elem.y << ":" << elem.color << std::endl;
-	}
+	temp.push_back(points[i]);
 
-	// If modified array of points has less than 3 points,
-	// convex hull is not possible
-	if (temp.size() < 3) return;
-
-	// Create an empty stack and push first three points
-	// to it.
-	stack<Point> S;
-	S.push(points[0]);
-	S.push(points[1]);
-	S.push(points[2]);
-
-	// Process remaining n-3 points
-	for (int i = 3; i < temp.size(); i++)
-	{
-		// Keep removing top while the angle formed by
-		// points next-to-top, top, and points[i] makes
-		// a non-left turn
-		while (S.size() > 1 && orientation(nextToTop(S), S.top(), points[i]) != 2)
-			S.pop();
-		S.push(points[i]);
-	}
-
-	// Now stack has the output points, print contents of stack
-	while (!S.empty())
-	{
-		Point p = S.top();
-		cout << p.x << ";" << p.y << endl;
-		S.pop();
-	}
 }
 
-void ReadFile(std::string file_name, std::vector<Point> &points) {
-	int row = 0;
+for (auto& elem : temp) {
+	std::cout << elem.x << ":" << elem.y << ":" << elem.color << std::endl;
+}
+
+// If modified array of points has less than 3 points,
+// convex hull is not possible
+if (temp.size() < 3) return;
+
+// Create an empty stack and push first three points
+// to it.
+stack<Point> S;
+S.push(points[0]);
+S.push(points[1]);
+S.push(points[2]);
+
+// Process remaining n-3 points
+for (int i = 3; i < temp.size(); i++)
+{
+	// Keep removing top while the angle formed by
+	// points next-to-top, top, and points[i] makes
+	// a non-left turn
+	while (S.size() > 1 && orientation(nextToTop(S), S.top(), points[i]) != 2)
+		S.pop();
+	S.push(points[i]);
+}
+
+// Now stack has the output points, print contents of stack
+while (!S.empty())
+{
+	Point p = S.top();
+	cout << p.x << ";" << p.y << endl;
+	S.pop();
+}
+}
+
+int getNumOfLines(std::string file_name) {
+	std::ifstream file(file_name);
+	std::string str;
+	int number_of_lines = 0;
+
+	while (std::getline(file, str)) {
+		number_of_lines++;
+	}
+	file.close();
+
+	return number_of_lines;
+}
+
+void ReadFile(std::string file_name, std::vector<Point>& points, int numLines) {
+	int y = (std::floor((double)numLines / 2));
+	
 
 	// Opens the file for reading
 	std::ifstream file(file_name);
@@ -181,15 +196,24 @@ void ReadFile(std::string file_name, std::vector<Point> &points) {
 
 	// Iterates over the file, storing one line at a time into `str`
 	while (std::getline(file, str)) {
-		
+		int start = -(std::floor((double)str.length() / 2));
+		int end = (std::floor((double)str.length() / 2));
+		int s = 0;
+
 		//pushes each binary value into points
-		for (int i = 0; i < str.length(); i++) {
-			
-			Point temp(row, i, (str[i] - '0'));
+		for (int i = start; i <= end; i++) {
+			if (i == 0 && str.size() % 2 == 0) {
+				continue;
+			}
+			if (y == 0 && numLines % 2 == 0) {
+				y--;
+			}	
+			Point temp(i, y, (str[s] - '0'));
 			points.push_back(temp);
+			s++;
 		}
 
-		row++;
+		y--;
 	}
 }
 
@@ -201,9 +225,11 @@ int main(int argc, char* argv[])
 	std::vector<Point> p;
 	std::string file_name = argv[2];
 
-	ReadFile(file_name, p);
+	int numberLines = getNumOfLines(file_name);
+	ReadFile(file_name, p, numberLines);
 	
 	convexHull(p);
-
+	
+	std::cout << p0.x << p0.y << std::endl;
 	return 0;
 }
