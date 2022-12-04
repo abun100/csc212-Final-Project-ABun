97d4fbb94fda0bba50420be86f0dc2e7bee45e33
mtabatneck <98777194+mtabatneck@users.noreply.github.com>
2022-12-04 11:05:05 -0500

    Update GJK.h

--- functions/simplex-Implement/GJK.h
+++ functions/simplex-Implement/GJK.h
@@ -1,19 +1,39 @@
 #pragma once
-#include <pair>
-struct Simplex {
-private :
-	std::vector<Points> simplex;
+#include "grahamScan.h"
+
 
 class GJK
 {
 private:
-	std::pair<int, int> origin;
+	//set of polygon points 
+	std::vector<Point> poly1;
+	std::vector<Point> poly2;
+	std::vector<Point> simplex;
+
+	//The clotest center to our convex shapes
+	Point center1;
+	Point center2;
+
+	//Direction vector
+	Point direction;
+
+	
 
 public:
-	bool gjk(Point poly1, Point poly2);
-	Point support(std::vector<std::pair<int, int>> direction);
-	Point handleSimplex(Simplex simeplex, std::vector<std::pair<int, int>> direction);
-	Point lineCase(Simplex simeplex, std::vector<std::pair<int, int>> direction);
-	Point triangleCase(Simplex simeplex, std::vector<std::pair<int, int>> direction);
-};
 
+	//Util functions to help find simplex points on minkowski sum
+	void find_centroid();
+	Point find_furthest_point(std::vector<Point>& outer_hull, Point& direction);
+	float dot(Point& p, Point& d);
+	float cross();
+	Point negative_direction(Point d);
+	Point subtract(Point a, Point b);
+
+
+	//Main Function of GJK
+	void gjk();
+	void check_simplex();
+	void line_case();
+	void triangel_case();
+
+};
